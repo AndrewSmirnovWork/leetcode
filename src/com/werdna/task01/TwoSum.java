b26bce738e055523946bd9853b3bc336cf4bc946
andrewsmirnovwork <icar08@yandex.ru>
2020-04-09 15:56:11 +0300

    first tasks from leetcode

---
+++ src/com/werdna/task01/TwoSum.java
@@ -0,0 +1,46 @@
+package com.werdna.task01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * <p>
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+public class TwoSum {
+
+    //Brute force approach
+    //Time complexity O(n^2)
+    //Space complexity O(1)
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two Sum numbers");
+    }
+
+    public int[] twoSumWithMap(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0 ; i <nums.length; i++) {
+            int result = target - nums[i];
+            if (map.containsKey(result)) {
+                return new int[]{map.get(result), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two Sum numbers");
+    }
+}
---
+++ src/com/werdna/task02/ListNode.java
@@ -0,0 +1,10 @@
+package com.werdna.task02;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+    }
+}
